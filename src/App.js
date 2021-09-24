7a6ecf7d5e2084e20ff53d25c42251a72b091f8d
antonianwufo <antonia.nwufo@haymarket.com>
2021-09-24 16:18:27 +0100

    React Restaurant App - 1st draft

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Restaurants from './components/Restaurants';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const [restaurants] = useState([
+{
+  id: 1,
+  title: 'Wagamama',
+},
+{
+  id: 2,
+  title: 'Benihana',
+},
+{
+  id: 3,
+  title: 'Nobu',
+}
+])
+
+return (
+  <divm className='container'>
+    <Header />
+    <Restaurants restaurants={restaurants} />
+  {console.log(restaurants)}
+  </divm>
   );
 }
 
---
+++ src/components/Restaurant.js
@@ -0,0 +1,11 @@
+
+
+const Restaurant = () => {
+    return (
+        <div className={`restaurant`}>
+       <h3>{Restaurant.title}</h3>
+    </div>
+    )
+}
+
+export default Restaurant
